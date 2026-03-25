3e099b5f136a63626f47df099dc2bad89e217396
MarkBeB <177214717+MarkBeB@users.noreply.github.com>
2026-03-25 21:02:24 +0100

    Improve local runner

--- org.tud.minitimetable/src/main/java/org/tud/minitimetable/LocalRunner.java
+++ org.tud.minitimetable/src/main/java/org/tud/minitimetable/LocalRunner.java
@@ -1,25 +1,55 @@
 package org.tud.minitimetable;
 
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 
 import org.tud.minitimetable.extern.solver.MiniZinc;
-import org.tud.minitimetable.util.PathUtils;
+import org.tud.minitimetable.extern.solver.ProcessLogger.DefaultFileLog;
+import org.tud.minitimetable.extern.validator.ValidatorRunner;
+import org.tud.minitimetable.model.util.SolutionFileReader;
 
 public class LocalRunner {
 
-	public static void main(String[] args) throws IOException {
-		Path resourceDirectory = Path.of("./", "resources");
-		Path modelFile = resourceDirectory.resolve("minizinc").resolve("HardAndSomeSoft.mzn");
-		Path dataFile = resourceDirectory.resolve("input").resolve("ihtc").resolve("i01.json");
-		Path outputFolder = resourceDirectory.resolve("out")
-				.resolve(PathUtils.removeFileExtension(dataFile.getFileName()));
+	public static void main(String[] args) throws IOException, InterruptedException {
+		Path resourceDirectory = Path.of("./", "resources").toAbsolutePath();
+		Path modelFile = resourceDirectory.resolve("minizinc").resolve("AllConstraints.mzn");
+		Path dataFile = resourceDirectory.resolve("input").resolve("ihtc").resolve("i02.json");
+		Path outputFolder = resourceDirectory.resolve("out").resolve("i02-4");
 
 		MiniZinc minizinc = new MiniZinc();
 		DefaultSettings.applyDefaultMiniZincConfiguration(minizinc);
-		minizinc.getConfig().timeLimitMS = 2 * 60 * 1000l;
+		minizinc.getConfig().timeLimitMS = 5 * 60 * 1000l;
 
 		minizinc.run(modelFile, dataFile, outputFolder).join();
+
+		var solutionOutputFile = ((DefaultFileLog) minizinc.getConfig().solverOutput).getPath();
+		if (Files.exists(solutionOutputFile)) {
+			var reader = new SolutionFileReader();
+			var solutions = reader.parseSolutionFile(solutionOutputFile);
+			if (solutions.size() > 0) {
+				var best = solutions.getLast();
+				var file = outputFolder.resolve(reader.constructNameForSolution(best));
+				reader.writeToFile(best, file);
+
+				System.out.println(String.format("Validating file '%s' with solution '%s'", dataFile, file));
+				ValidatorRunner validator = new ValidatorRunner(resourceDirectory.resolve("IHTP_Validator_Win11.exe"),
+						outputFolder);
+				validator.run(dataFile, file);
+			}
+		}
+	}
+
+	public static void main2(String[] args) throws IOException, InterruptedException {
+		Path resourceDirectory = Path.of("./", "resources").toAbsolutePath();
+		Path dataFile = resourceDirectory.resolve("input").resolve("ihtc").resolve("i02.json");
+		Path outputFolder = resourceDirectory.resolve("out").resolve("i02-4");
+		Path solutionFile = outputFolder.resolve("solution#1_1464.json");
+
+		System.out.println(String.format("Validating file '%s' with solution '%s'", dataFile, solutionFile));
+		ValidatorRunner validator = new ValidatorRunner(resourceDirectory.resolve("IHTP_Validator_Win11.exe"),
+				outputFolder);
+		validator.run(dataFile, solutionFile);
 	}
 
 }
