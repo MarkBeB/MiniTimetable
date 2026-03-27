76814a9540a37ce6b6077a45e91f04da6471a682
MarkBeB <177214717+MarkBeB@users.noreply.github.com>
2026-03-27 04:55:44 +0100

    Update LocalRunner.java

--- org.tud.minitimetable/src/main/java/org/tud/minitimetable/LocalRunner.java
+++ org.tud.minitimetable/src/main/java/org/tud/minitimetable/LocalRunner.java
@@ -14,11 +14,12 @@ import org.tud.minitimetable.util.PathUtils;
 
 public class LocalRunner {
 
-	public static void main(String[] args) throws IOException, InterruptedException {
-		Path resourceDirectory = Path.of("./", "resources").toAbsolutePath();
-		Path modelFile = resourceDirectory.resolve("minizinc").resolve("AllConstraints_opt2.mzn");
+	private static final Path resourceDirectory = Path.of("./", "resources").toAbsolutePath();
+
+	public static void main2(String[] args) throws IOException, InterruptedException {
+		Path modelFile = resourceDirectory.resolve("minizinc").resolve("AllConstraintsV2.mzn");
 		Path dataFile = resourceDirectory.resolve("input").resolve("ihtc").resolve("i02.json");
-		Path outputFolder = resourceDirectory.resolve("out").resolve("i02-opt5");
+		Path outputFolder = resourceDirectory.resolve("out").resolve("i02-base2");
 
 		MiniZinc minizinc = new MiniZinc();
 		DefaultSettings.applyDefaultMiniZincConfiguration(minizinc);
@@ -30,32 +31,33 @@ public class LocalRunner {
 		((FileCodeLogger) minizinc.getConfig().logger).close();
 
 		var solutionOutputFile = ((DefaultFileLog) minizinc.getConfig().solverOutput).getPath();
-		if (Files.exists(solutionOutputFile)) {
-			var reader = new SolutionFileReader();
-			var solutions = reader.parseSolutionFile(solutionOutputFile);
-			if (solutions.size() > 0) {
-				var best = solutions.getLast();
-				var file = outputFolder.resolve(reader.constructNameForSolution(best));
-				reader.writeToFile(best, file);
-
-				System.out.println(String.format("Validating file '%s' with solution '%s'", dataFile, file));
-				ValidatorRunner validator = new ValidatorRunner(resourceDirectory.resolve("IHTP_Validator_Win11.exe"),
-						outputFolder);
-				validator.run(dataFile, file);
-			}
-		}
+		if (Files.exists(solutionOutputFile))
+			processSoltions(solutionOutputFile, dataFile);
 	}
 
-	public static void main2(String[] args) throws IOException, InterruptedException {
-		Path resourceDirectory = Path.of("./", "resources").toAbsolutePath();
-		Path dataFile = resourceDirectory.resolve("input").resolve("ihtc").resolve("i02.json");
-		Path outputFolder = resourceDirectory.resolve("out").resolve("i02-4");
-		Path solutionFile = outputFolder.resolve("solution#1_1464.json");
+	public static void main(String[] args) throws IOException, InterruptedException {
+		Path dataFile = resourceDirectory.resolve("input").resolve("ihtc").resolve("i01.json");
+		Path outputFolder = resourceDirectory.resolve("out").resolve("i01-long2");
+		Path solutionFile = outputFolder.resolve("i01-solution.json");
 
-		System.out.println(String.format("Validating file '%s' with solution '%s'", dataFile, solutionFile));
-		ValidatorRunner validator = new ValidatorRunner(resourceDirectory.resolve("IHTP_Validator_Win11.exe"),
-				outputFolder);
-		validator.run(dataFile, solutionFile);
+		processSoltions(solutionFile, dataFile);
+	}
+
+	private static void processSoltions(Path solutionFile, Path dataFile) throws IOException, InterruptedException {
+		var reader = new SolutionFileReader();
+		var solutions = reader.parseSolutionFile(solutionFile);
+
+		if (solutions.size() > 0) {
+			var best = solutions.getLast();
+			var file = solutionFile.resolveSibling(reader.constructNameForSolution(best));
+			reader.writeToFile(best, file);
+
+			System.out.println(String.format("Validating file '%s' with solution '%s'", dataFile, file));
+			ValidatorRunner validator = new ValidatorRunner(resourceDirectory.resolve("IHTP_Validator_Win11.exe"),
+					solutionFile.getParent());
+			validator.run(dataFile, file);
+		}
+
 	}
 
 }
