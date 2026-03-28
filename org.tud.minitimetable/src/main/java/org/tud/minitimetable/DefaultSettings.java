8e2ab651f5bb56830cc0c05411a431c8cabd3dc3
MarkBeB <177214717+MarkBeB@users.noreply.github.com>
2026-03-28 19:07:46 +0100

    Update DefaultSettings.java

--- org.tud.minitimetable/src/main/java/org/tud/minitimetable/DefaultSettings.java
+++ org.tud.minitimetable/src/main/java/org/tud/minitimetable/DefaultSettings.java
@@ -18,7 +18,7 @@ public class DefaultSettings {
 	public static void applyDefaultMiniZincConfiguration(MiniZinc minizinc) {
 		minizinc.getConfig().backendLog = new DefaultFileLog("%s-backend");
 		minizinc.getConfig().solverOutput = new DefaultFileLog("%s-solution");
-		minizinc.getConfig().solverModelLog = new DefaultFileLog("%s-lp.lp", true);
+//		minizinc.getConfig().solverModelLog = new DefaultFileLog("%s-lp.lp", true);
 
 		minizinc.getConfig().timeLimitMS = DefaultSettings.DEFAULT_TIME_LIMIT_IN_MS;
 		minizinc.getConfig().threads = DefaultSettings.DEFAULT_NUMBER_OF_THREADS;
